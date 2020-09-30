67f5e3a65f8b464f2feeb6486dd35c6c3a59d481
BirkBerger <theabberger@gmail.com>
2020-09-30 14:01:39 +0200

    finished data hazard solve algo and have gotten a few tests accepted

--- features/step_definitions/assembly_parser_steps.js
+++ features/step_definitions/assembly_parser_steps.js
@@ -4,13 +4,10 @@ const fs = require('fs')
 const AssemblyParser = require('/Users/birkberger/Documents/DTU/Bachelorprojekt/bachelorThesis2/lib/assemblyParser');
 const ErrorMessageHolder = require('/Users/birkberger/Documents/DTU/Bachelorprojekt/bachelorThesis2/lib/errorMessageHolder');
 
-
-
 let errorMessageHolder;
 
-
 Given("that the assembly editor holds the content of {string}", function (textFileName) {
-    let code = fs.readFileSync("features/test_files_assembly_editor/" + textFileName).toString();
+    let code = fs.readFileSync("features/test_files/assembly_editor/" + textFileName).toString();
     let assemblyParser = new AssemblyParser(code);
     let errorMessage = assemblyParser.getAllInstructionErrors();
     errorMessageHolder = new ErrorMessageHolder(errorMessage);
--- features/step_definitions/hazard_solver_steps.js
+++ features/step_definitions/hazard_solver_steps.js
@@ -1,30 +1,52 @@
 const {After, Before, Given, When, Then } = require('cucumber');
 const assert = require('assert');
 const fs = require('fs')
-// const AssemblyParser = require('/Users/birkberger/Documents/DTU/Bachelorprojekt/bachelorThesis2/lib/assemblyParser');
+const HazardSolver = require("../../lib/hazardSolver");
+const AssemblyCompiler = require("../../lib/assemblyCompiler");
+const HazardSolverHolder = require("../../lib/hazardSolverHolder");
+const CompilerHolder = require("../../lib/compilerHolder");
+const CodeHolder = require("../../lib/codeHolder");
+const AssemblyParser = require("../../lib/assemblyParser");
+const ParserHolder = require("../../lib/parserHolder");
 
+let codeHolder;
+let parserHolder;
+let compilerHolder;
+let hazardSolverHolder;
+
+
+Given("that the assembly editor holds the input with no errors: {string}", function (textFileName) {
+    let code = fs.readFileSync("features/test_files/pipeline_table/" + textFileName).toString();
+    codeHolder = new CodeHolder(code);
+    let parser = new AssemblyParser(code);
+    parser.getAllInstructionErrors();
+    parserHolder = new ParserHolder(parser);
+});
+
+Given('the assembly code is compiled', function () {
+    let compiler = new AssemblyCompiler();
+    compiler.storeCode(codeHolder.getCode(), parserHolder.getParser().instructionSignals);
+    compilerHolder = new CompilerHolder(compiler);
+});
 
 Given('the data hazards are removed from the assembly code', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    let hazardSolver = new HazardSolver(compilerHolder.getCompiler().cleanAssemblyCode);
+    hazardSolver.solveAllHazards();
+    hazardSolverHolder = new HazardSolverHolder(hazardSolver);
 });
 
-Then('{int} data hazards are detected', function (int) {
-    // Then('{float} data hazards are detected', function (float) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+
+Then('{int} data hazards are detected', function (expectedAmount) {
+    assert.equal(hazardSolverHolder.getHazardSolver().numberOfHazards, expectedAmount);
 });
 
-Then('there is a forwarding line from line {int} at cc {float} to line {int} at {float}', function (int, float, int2, float2) {
-    // Then('there is a forwarding line from line {int} at cc {float} to line {float} at {float}', function (int, float, float2, float3) {
-    // Then('there is a forwarding line from line {float} at cc {float} to line {int} at {float}', function (float, float2, int, float3) {
-    // Then('there is a forwarding line from line {float} at cc {float} to line {float} at {float}', function (float, float2, float3, float4) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+Then('there is a forwarding line from line {int} at cc {float} to line {int} at {float}', function (srcLine, srcCC, desLine, desCC) {
+    let noHazardsAssemblyCode = hazardSolverHolder.getHazardSolver().noHazardsAssemblyCode;
+    console.log(noHazardsAssemblyCode);
+    assert((noHazardsAssemblyCode[srcLine][5].toString()).indexOf(srcCC + "," + desLine + "," + desCC) > -1);
 });
 
-Then('there is a stall at line {int}', function (int) {
-    // Then('there is a stall at line {float}', function (float) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+Then('there is a stall at line {int}', function (lineNumber) {
+    let noHazardsAssemblyCode = hazardSolverHolder.getHazardSolver().noHazardsAssemblyCode;
+    assert(noHazardsAssemblyCode[lineNumber] = "---");
 });
