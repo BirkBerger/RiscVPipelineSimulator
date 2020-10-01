6a03e97b759f20e23efa43c17a7487a5677b71c8
BirkBerger <theabberger@gmail.com>
2020-10-01 14:51:29 +0200

    Stalls (as text stall)  are now inserted in table when solve my hazards button is clicked

--- features/step_definitions/assembly_parser_steps.js
+++ features/step_definitions/assembly_parser_steps.js
@@ -2,7 +2,7 @@ const {After, Before, Given, When, Then } = require('cucumber');
 const assert = require('assert');
 const fs = require('fs')
 const AssemblyParser = require('/Users/birkberger/Documents/DTU/Bachelorprojekt/bachelorThesis2/lib/assemblyParser');
-const ErrorMessageHolder = require('/Users/birkberger/Documents/DTU/Bachelorprojekt/bachelorThesis2/lib/errorMessageHolder');
+const ErrorMessageHolder = require('/Users/birkberger/Documents/DTU/Bachelorprojekt/bachelorThesis2/lib/holders/errorMessageHolder');
 
 let errorMessageHolder;
 
--- features/step_definitions/hazard_solver_steps.js
+++ features/step_definitions/hazard_solver_steps.js
@@ -3,9 +3,9 @@ const assert = require('assert');
 const fs = require('fs')
 const HazardSolver = require("../../lib/hazardSolver");
 const AssemblyCompiler = require("../../lib/assemblyCompiler");
-const HazardSolverHolder = require("../../lib/hazardSolverHolder");
-const CompilerHolder = require("../../lib/compilerHolder");
-const CodeHolder = require("../../lib/codeHolder");
+const HazardSolverHolder = require("../../lib/holders/hazardSolverHolder");
+const CompilerHolder = require("../../lib/holders/compilerHolder");
+const CodeHolder = require("../../lib/holders/codeHolder");
 const AssemblyParser = require("../../lib/assemblyParser");
 const ParserHolder = require("../../lib/parserHolder");
 
@@ -31,22 +31,30 @@ Given('the assembly code is compiled', function () {
 
 Given('the data hazards are removed from the assembly code', function () {
     let hazardSolver = new HazardSolver(compilerHolder.getCompiler().cleanAssemblyCode);
+    hazardSolver.detectAllHazards();
     hazardSolver.solveAllHazards();
     hazardSolverHolder = new HazardSolverHolder(hazardSolver);
 });
 
 
 Then('{int} data hazards are detected', function (expectedAmount) {
+    // console.log(hazardSolverHolder.getHazardSolver().hazardFreeCode);
     assert.equal(hazardSolverHolder.getHazardSolver().numberOfHazards, expectedAmount);
 });
 
 Then('there is a forwarding line from line {int} at cc {float} to line {int} at {float}', function (srcLine, srcCC, desLine, desCC) {
-    let noHazardsAssemblyCode = hazardSolverHolder.getHazardSolver().noHazardsAssemblyCode;
-    console.log(noHazardsAssemblyCode);
+    let noHazardsAssemblyCode = hazardSolverHolder.getHazardSolver().hazardFreeCode;
     assert((noHazardsAssemblyCode[srcLine][5].toString()).indexOf(srcCC + "," + desLine + "," + desCC) > -1);
 });
 
 Then('there is a stall at line {int}', function (lineNumber) {
-    let noHazardsAssemblyCode = hazardSolverHolder.getHazardSolver().noHazardsAssemblyCode;
-    assert(noHazardsAssemblyCode[lineNumber] = "---");
+    let hazardFreeAssemblyCode = hazardSolverHolder.getHazardSolver().hazardFreeCode;
+    assert(hazardFreeAssemblyCode[lineNumber] = "---");
+});
+
+Then('there are no forwarding lines', function () {
+    let hazardFreeAssemblyCode = hazardSolverHolder.getHazardSolver().hazardFreeCode;
+    for (var i = 0; i < hazardFreeAssemblyCode.length; i++) {
+        assert(hazardFreeAssemblyCode[5] = []);
+    }
 });
