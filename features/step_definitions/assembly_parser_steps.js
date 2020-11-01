e24992a72bff4459702a7a337abf0bcf74448c2c
BirkBerger <theabberger@gmail.com>
2020-11-01 22:00:38 +0100

    website is now deployed and adjusts to mobile

--- features/step_definitions/assembly_parser_steps.js
+++ features/step_definitions/assembly_parser_steps.js
@@ -1,8 +1,8 @@
 const {After, Before, Given, When, Then } = require('cucumber');
 const assert = require('assert');
 const fs = require('fs')
-const AssemblyParser = require('/Users/birkberger/Documents/DTU/Bachelorprojekt/bachelorThesis2/lib/assemblyParser');
-const ErrorMessageHolder = require('/Users/birkberger/Documents/DTU/Bachelorprojekt/bachelorThesis2/lib/holders/errorMessageHolder');
+const AssemblyParser = require('/Users/birkberger/Documents/DTU/Bachelorprojekt/RiscVPipelineSimulator/lib/assemblyParser');
+const ErrorMessageHolder = require('/Users/birkberger/Documents/DTU/Bachelorprojekt/RiscVPipelineSimulator/lib/holders/errorMessageHolder');
 
 let errorMessageHolder;
 
